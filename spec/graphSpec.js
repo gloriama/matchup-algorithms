a60ace51d56b2644ef8ab47f91c96a7bd3680443
Gloria Ma <gloria.ma@aya.yale.edu>
2016-04-25 15:43:54 -0700

    Add tests for addNode, removeNode, and getComponents

--- spec/graphSpec.js
+++ spec/graphSpec.js
@@ -1,3 +1,63 @@
-var expect = require('chai').expect;
+var assert = require('chai').assert;
+var Graph = require('../graph.js');
 
-expect('hello').to.be.a('number');
+describe('Graph', function() {
+  var graph;
+
+  // Initialize the graph newly before each "it" block below
+  beforeEach(function() {
+    graph = new Graph();
+    for (var i = 0; i < 100; i++) {
+      graph.addNode(i);
+      for (var j = 0; j < 10; j++) {
+        graph.addEdge(i, Math.floor(Math.random() * i));
+      };
+    }
+    // console.log(graph);
+  });
+
+  describe('addNode()', function () {
+    it('should return an object if the node already existed', function () {
+      assert(true, typeof graph.addNode(10) === 'object');
+    });
+    it('should return an object if the node didn\'t already exist', function () {
+      assert(true, typeof graph.addNode(1000) === 'object');
+    });
+    it('should add the node value as a key in the graph\'s edges object', function() {
+      for (var i = 0; i < 100; i++) {
+        assert.isTrue(i in graph.edges);
+      }
+    });
+    it('should not add any other value as a key in the graph\'s edges object', function() {
+      assert.isFalse(2000 in graph.edges);
+    });
+  });
+
+  describe('removeNode()', function () {
+    it('should return true if the node existed', function () {
+      assert.isTrue(graph.removeNode(10));
+    });
+    it('should return false if the node didn\'t exist', function () {
+      graph.removeNode(10);
+      assert.isFalse(graph.removeNode(10));
+    });
+    it('should remove the node value as a key from the graph\'s edges object', function() {
+      graph.removeNode(10);
+      assert.isFalse(10 in graph.edges);
+    });
+  });
+
+  describe('getComponents()', function () {
+    it('should return an array', function () {
+      var components = graph.getComponents();
+      assert.equal(true, Array.isArray(components));
+    });
+    it('should return an array of arrays', function () {
+      var components = graph.getComponents();
+      // console.log(components);
+      components.forEach(function(component) {
+        assert.equal(true, Array.isArray(component));
+      });
+    });
+  });
+});
